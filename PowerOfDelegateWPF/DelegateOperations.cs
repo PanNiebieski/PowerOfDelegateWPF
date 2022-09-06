c186798c73526998290724e9c29cfb49a918dc2f
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2022-09-06 09:39:00 +0200

    Add project files.

---
+++ PowerOfDelegateWPF/DelegateOperations.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerOfDelegateWPF
+{
+    public delegate int DelegateOneOperationRunner(int a, int b);
+    public delegate void DelegateOperationsRunner(int a, int b);
+
+    public class OneOperationRunner
+    {
+        public DelegateOneOperationRunner Run(Operation operation)
+        {
+            DelegateOneOperationRunner runner = null;
+
+            if (operation == Operation.Add)
+                runner += Add;
+
+            if (operation == Operation.Substrack)
+                runner += Substract;
+
+            if (operation == Operation.Mutyply)
+                runner += Multyply;
+
+            if (operation == Operation.Supply)
+                runner += Supply;
+
+            return runner;
+        }
+
+        private int Add(int a, int b) { return a + b; }
+        private int Substract(int a, int b) { return a - b; }
+        private int Multyply(int a, int b) { return a * b; }
+        private int Supply(int a, int b) { return a / b; }
+
+    }
+
+    public class OperationsRunner
+    {
+        private DelegateOperationsRunner runner = null;
+
+        public void AddOperation(Operation operation)
+        {
+            if (operation == Operation.Add)
+                runner += Add;
+
+            if (operation == Operation.Substrack)
+                runner += Substract;
+
+            if (operation == Operation.Mutyply)
+                runner += Multyply;
+
+            if (operation == Operation.Supply)
+                runner += Supply;
+        }
+
+        public void Run(int a, int b)
+        {
+            if (runner == null)
+                return;
+
+            runner.Invoke(a, b);
+        }
+
+        public int Result
+        {
+            get;
+            set;
+        }
+
+        private void Add(int a, int b)
+        {
+            if (Result == 0) { Result = a + b; } else { Result = Result + b; }
+        }
+        private void Substract(int a, int b)
+        {
+            if (Result == 0) { Result = a - b; } else { Result = Result - b; }
+        }
+        private void Multyply(int a, int b)
+        {
+            if (Result == 0) { Result = a * b; } else { Result = Result * b; }
+        }
+        private void Supply(int a, int b)
+        {
+            if (Result == 0) { Result = a / b; } else { Result = Result / b; }
+        }
+    }
+
+    public enum Operation
+    {
+        Add,
+        Substrack,
+        Mutyply,
+        Supply
+    }
+}
---
+++ PowerOfDelegateWPF/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PowerOfDelegateWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+private void Button_AddOperaction_Click(object sender, RoutedEventArgs e)
+{
+    Button b = (Button)e.Source;
+    string s = "";
+    if (b.Tag.ToString() == "Plus")
+    {
+            s = "Dodawanie :" + txtNumber.Text;
+    }
+    if (b.Tag.ToString() == "Minus")
+    {
+        s = "Odejmowanie :" + txtNumber.Text;
+    }
+    if (b.Tag.ToString() == "Dzie")
+    {
+        s = "Dzielenie :" + txtNumber.Text;
+    }
+    if (b.Tag.ToString() == "Mno")
+    {
+        s = "Mnożenie :" + txtNumber.Text;
+    }
+
+    AddToListBox(s);
+}
+
+public void AddToListBox(string text)
+{
+    lbOperations.Items.Add(text);
+}
+
+        public void AddToTheLast(string text)
+        {
+            if (lbOperations.Items.Count > 0)
+            {
+                string t = lbOperations.Items[lbOperations.Items.Count - 1].ToString();
+
+                t = t + ":" + text;
+
+                lbOperations.Items[lbOperations.Items.Count - 1] = t;
+            }
+            else
+            {
+                AddToTheLast(text);
+            }
+
+                
+        }
+
+private void Button_Run_Click(object sender, RoutedEventArgs e)
+{
+    OneOperationRunner runner = new OneOperationRunner();
+    OperationsRunner r = new OperationsRunner();
+
+    int a = 0; //Startowa wartość
+
+    foreach (string item in lbOperations.Items)
+    {
+        var tab = item.Split(':');
+
+        int b = int.Parse(tab[1]);
+
+        DelegateOneOperationRunner del = runner.Run(Operation.Add);
+
+        if (item.Contains("Dodawanie"))
+        {
+            del = runner.Run(Operation.Add);
+
+            r.AddOperation(Operation.Add);
+        }
+
+        if (item.Contains("Odejmowanie"))
+        {
+            del = runner.Run(Operation.Substrack);
+
+            r.AddOperation(Operation.Substrack);
+        }
+
+        if (item.Contains("Mnożenie"))
+        {
+            del = runner.Run(Operation.Mutyply);
+
+            r.AddOperation(Operation.Mutyply);
+        }
+
+        if (item.Contains("Dzielenie"))
+        {
+            del = runner.Run(Operation.Supply);
+
+            r.AddOperation(Operation.Supply);
+        }
+
+        //Wykoanie operacji pojedyńczo
+        a = del.Invoke(a, b);
+    }
+
+    //Wykonanie operacji na raz
+    r.Run(0, int.Parse(txtNumber.Text));
+    int many = r.Result;
+    //Wyświetlenie wyników
+    lblResult.Content = "Po koleji: " + a.ToString();
+    lblResult_2.Content = "Wszystkie operacje na raz na zmiennej w polu tekstowym: " + many.ToString();
+}
+
+
+
+        private void Button_Run_Many_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
